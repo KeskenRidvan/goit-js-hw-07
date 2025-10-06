9dfed1633b62520941c2078656a79b3e6ded42ab
Ridvan Kesken <kesken.dev@gmail.com>
2025-10-06 12:39:15 +0300

    task-4: completed.

--- src/js/task-4.js
+++ src/js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+const handleLoginForm = (event) => {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+  const emailValue = elements.email.value.trim();
+  const passwordValue = elements.password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const userInfo = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(userInfo);
+
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", handleLoginForm);
