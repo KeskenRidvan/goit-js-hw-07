ed0d5a11c31389abb433ddeb24d8e16d6e767969
Ridvan Kesken <kesken.dev@gmail.com>
2025-10-06 12:32:19 +0300

    fix: The name of the inputHandle method has been updated to handleInput.

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -3,9 +3,9 @@ const nameOutput = document.querySelector("#name-output");
 
 console.log(nameInput, nameOutput);
 
-const inputHandle = (event) => {
+const handleInput = (event) => {
   const trimmedName = event.currentTarget.value.trim();
   nameOutput.textContent = trimmedName || "Anonymous";
 };
 
-nameInput.addEventListener("input", inputHandle);
+nameInput.addEventListener("input", handleInput);
